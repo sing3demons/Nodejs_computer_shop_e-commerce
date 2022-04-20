85a699326172a42e8cffa6412da7500f66f2787c
sing3demons <sing3demons@live.com>
2022-04-20 12:08:10 +0700

    build: dockerfile tag0.2

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const passport = require('passport');
 const MongoStore = require('connect-mongo')(session);
 const mongoose = require('mongoose');
 const config = require('./config/index')
-//ชำระเงิน
+    //ชำระเงิน
 const stripe = require('stripe')(config.PAY_STRIPE);
 
 //router
@@ -23,8 +23,8 @@ const app = express();
 
 app.set('trust proxy', 1);
 const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 1000 // limit each IP to 100 requests per windowMs
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 1000 // limit each IP to 100 requests per windowMs
 });
 //  apply to all requests
 app.use(limiter);
@@ -32,10 +32,10 @@ app.use(helmet());
 
 // connect mongodb
 mongoose.connect(config.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
 });
 
 
@@ -44,21 +44,25 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
-app.use(express.json({ limit: '50mb' }));
-app.use(express.urlencoded({ extended: false }));
+app.use(express.json({
+    limit: '50mb'
+}));
+app.use(express.urlencoded({
+    extended: false
+}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(session({
-  name: 'sessionId',
-  secret: config.SECRET_KEY,
-  saveUninitialized: false, // don't create sessions for not logged in users
-  resave: false, //don't save session if unmodified
+    name: 'sessionId',
+    secret: config.SECRET_KEY,
+    saveUninitialized: false, // don't create sessions for not logged in users
+    resave: false, //don't save session if unmodified
 
-  // Where to store session data
-  store: new MongoStore({
-    mongooseConnection: mongoose.connection,
-    ttl: 1 * 24 * 60 * 60 // = 14 days. ttl means "time to live" (expiration in seconds)
-  }),
+    // Where to store session data
+    store: new MongoStore({
+        mongooseConnection: mongoose.connection,
+        ttl: 1 * 24 * 60 * 60 // = 14 days. ttl means "time to live" (expiration in seconds)
+    }),
 }));
 
 // Passport Config
@@ -66,15 +70,15 @@ require('./middleware/passport')(passport);
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.use(function (req, res, next) {
-  res.locals.user = req.user || null;
-  next();
+app.use(function(req, res, next) {
+    res.locals.user = req.user || null;
+    next();
 })
 
 app.use(require('connect-flash')());
-app.use(function (req, res, next) {
-  res.locals.messages = require('express-messages')(req, res);
-  next();
+app.use(function(req, res, next) {
+    res.locals.messages = require('express-messages')(req, res);
+    next();
 });
 
 app.use('/', indexRouter);
@@ -83,42 +87,42 @@ app.use('/category', categoryRouter)
 app.use('/shop', shopRouter);
 
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  next(createError(404));
+app.use(function(req, res, next) {
+    next(createError(404));
 });
 
 //function จำกัดตัวอักษร ใช้แสดงรายละเอียด
 app.locals.descriptionText = (text, length) => {
-  return text.substring(0, length)
+    return text.substring(0, length)
 }
 
 
 app.locals.formatNumber = (number) => {
-  return number.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,');
+    return number.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,');
 }
 
 app.post('/payment', (req, res) => {
-  const token = req.body.stripeToken
-  const amount = req.body.amount
-  const charge = stripe.charges.create({
-    amount: amount,
-    currency: "thb",
-    source: token
-  }, (err, charge) => {
-    if (err) throw err
-  })
-  req.session.destroy()
-  res.redirect('/')
-})
-// error handler
-app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === config.NODE_ENV ? err : {};
+        const token = req.body.stripeToken
+        const amount = req.body.amount
+        const charge = stripe.charges.create({
+            amount: amount,
+            currency: "thb",
+            source: token
+        }, (err, charge) => {
+            if (err) throw err
+        })
+        req.session.destroy()
+        res.redirect('/')
+    })
+    // error handler
+app.use(function(err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === config.NODE_ENV ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
--- dockerfile
+++ dockerfile
@@ -1,14 +1,34 @@
-FROM node:10
+# FROM node:12.18.1
 
-WORKDIR /usr/app
+# WORKDIR /usr/app
 
-COPY ./package.json ./
-#copy file [เริ่มต้น] => [สิ้นสุด] เอาไฟล์ทั้งหมดไป
+# COPY ["package.json", "package-lock.json*", "./"]
+
+# RUN npm install --production
+
+# COPY ./ ./ 
+
+# CMD ["npm","start"]
+
+FROM node:12.18.1 as build
+
+LABEL description="This is a multi-stage NodeJS image"
+WORKDIR /src
+COPY ["package.json", "package-lock.json*", "./"]
 RUN npm install
-COPY ./ ./ 
-#หลังจาก install แล้วก็จะทำการ เริ่มโปรแกรม
-CMD ["npm","start"]
+COPY . .
+
+FROM node:12.18.1-slim
+WORKDIR /src 
+COPY --from=build /src .
+EXPOSE 8080
+CMD ["npm", "start"]
+
 
 # docker build . -t nodejs:1.0 [nametag]:[versiontag]
 # -t คือ กำหนด tag ถ้าไม่กำหนด tag มันคือ default
 # docker run -p 2000:4000 [nametag]:[versiontag]
+
+# docker run -it --rm -d -p 27017:27017 --name nodejs-mongo mongo:latest
+# docker build -t combuy:0.1 .
+# docker run -it --rm -d -p 3000:3000 --env-file ./.env --link nodejs-mongo:db  --name nodejs-combuy combuy:0.2
